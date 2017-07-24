7e426b32be22cd641d635df29dca5e8db5406048
khaledhindawi <k.hindawi2001@gmail.com>
2017-07-24 00:29:45 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template,request
+app= Flask(__name__)
+
+
+@app.route("/")
+def load_page():
+	return render_template ("index.html")
+
+@app.route("/feeding")
+def feeding_page():
+	return render_template ("feeding.html")
+
+
+@app.route("/development")
+def development_page():
+	return render_template ("development.html")
+
+@app.route("/contact")
+def contact():
+	return render_template ("contact.html")
+
+
+
+if __name__=="__main__":
+	app.run() 
